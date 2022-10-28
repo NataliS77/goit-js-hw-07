71bc5ba26dbbd1373e448f0680c4eb46a9f0081c
NataliS77 <107251503+NataliS77@users.noreply.github.com>
2022-10-28 15:19:33 +0300

    task 7/2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,29 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery')
+const cardMarkup = createGalleryItemsMarkup(galleryItems);
+// galleryContainer.addEventListener('click', onOpenModalGallery)
+galleryContainer.insertAdjacentHTML('beforeend', cardMarkup);
+
+function createGalleryItemsMarkup (galleryItems){
+return galleryItems.map(({ preview, original, description }) => {
+
+    return `
+        
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+    `
+   
+    }).join('');
+}
+// function onOpenModalGallery(event) {
+//     event.preventDefault();
+   
+// }
+ let lightbox = new SimpleLightbox('.gallery a', {
+        captionsData:'alt',
+        captionSelector:'img',
+        captionDelay:250,
+    });
